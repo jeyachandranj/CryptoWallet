35b74423ac0fec67be82080c7a34b7879b2894f4
Jeyachandran J <125851197+jeyachandranj@users.noreply.github.com>
2024-11-29 20:38:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,12 @@ const axios = require('axios')
 const app = express();
 
 app.use(express.json());
-app.use(cors());  
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 const RAZORPAY_KEY = "rzp_test_zOZ8aPurnNX8g7"
 const RAZORPAY_SECRET = "4Qfo9bY0gtGlmA6biAtaNOtD"
